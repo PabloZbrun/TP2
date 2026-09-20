2496b1fa627503e6b8d2af085eaf1c6e6ca39e6e
PabloZbrun <pablozbrun27@gmail.com>
2026-09-20 17:40:14 -0300

    Add main function to main_gdb.c

--- gini-iteracion2-asm-gdb/main_gdb.c
+++ gini-iteracion2-asm-gdb/main_gdb.c
@@ -1,1 +1,17 @@
+main
 
+
+#include <stdio.h>
+
+int gini_mas_uno(float valor);
+
+int main(void) {
+    float x;
+    int resultado;
+
+    x = 42.7f;
+    resultado = gini_mas_uno(x);
+    printf("gini_mas_uno(%.1f) = %d\n", x, resultado);
+
+    return 0;
+}
