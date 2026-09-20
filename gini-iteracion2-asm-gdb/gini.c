cfcae2ed781a9b1ae276fd2be0cc0fd2662b973b
PabloZbrun <pablozbrun27@gmail.com>
2026-09-20 17:38:40 -0300

    Add gini_mas_uno function to call gini_asm

--- gini-iteracion2-asm-gdb/gini.c
+++ gini-iteracion2-asm-gdb/gini.c
@@ -1,1 +1,13 @@
+#include <stdint.h>
+#include <string.h>
 
+extern int gini_asm(long r1, long r2, long r3, long r4, long r5, long r6, int32_t bits);
+
+int gini_mas_uno(float valor) {
+    int32_t bits;
+    int resultado;
+
+    memcpy(&bits, &valor, sizeof(bits));
+    resultado = gini_asm(0, 0, 0, 0, 0, 0, bits);
+    return resultado;
+}
