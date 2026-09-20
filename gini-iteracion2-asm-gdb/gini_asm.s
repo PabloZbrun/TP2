9e9a246eb971d9133cfc4edd45473cdd2f89a8c3
PabloZbrun <pablozbrun27@gmail.com>
2026-09-20 17:39:00 -0300

    Implement gini_asm function to process float input

--- gini-iteracion2-asm-gdb/gini_asm.s
+++ gini-iteracion2-asm-gdb/gini_asm.s
@@ -1,1 +1,13 @@
+ .text
+    .globl gini_asm
+    .type gini_asm, @function
+gini_asm:
+    pushq %rbp
+    movq  %rsp, %rbp
+    movss 0x10(%rbp), %xmm0      # 7mo argumento (el float) desde el stack
+    cvttss2si %xmm0, %eax        # float -> int (trunca)
+    addl  $1, %eax               # +1
+    popq  %rbp
+    ret
 
+    .section .note.GNU-stack,"",@progbits
